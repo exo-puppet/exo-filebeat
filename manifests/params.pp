49fbf243ebcf405d6de0f4377931f4a630b0c52e
Vincent Sellier <vsellier@exoplatform.com>
2016-01-28 21:48:48 +0100

    Don't download nightly build anymore

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,7 @@
 class filebeat::params {
   # $download_file  = "logstash-${filebeat::version}-${::architecture}.deb"
-#   $download_url   = "https://download.elastic.co/beats/filebeat/filebeat_${filebeat::version}_${::architecture}.deb"
-  # Temporary use nightly builds to support multiline parsing
   $download_file    = "filebeat_${filebeat::version}_${::architecture}.deb"
-  # 1.1.0-nightlylatest
-  $download_url     = "https://beats-nightlies.s3.amazonaws.com/filebeat/${download_file}"
+  $download_url   = "https://download.elastic.co/beats/filebeat/${download_file}"
 
   $config_file      = '/etc/filebeat/filebeat.yml'
 }
